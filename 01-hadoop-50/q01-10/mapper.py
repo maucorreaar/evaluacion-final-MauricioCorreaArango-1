21c7e9538449f51357740972f0541d13f179b5a4
Mauricio Correa Arango <50936566+maucorreaar@users.noreply.github.com>
2020-02-13 19:57:42 -0500

    Solución del ejercicio
    Se actualiza directamente ya que desde el github de escritorio me mostró error de que no tenia permisos

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -2,3 +2,29 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+
+    ##
+    ## itera sobre cada linea de codigo recibida
+    ## a traves del flujo de entrada
+    ##
+    cadena =''
+    for line in sys.stdin:
+
+        ##
+        ## genera las tuplas palabra \tabulador 1
+        ## ya que es un conteo de palabras
+        ##
+
+        pos = 0
+        for word in list(line.split(',')):
+            pos +=1
+            ##
+            ## escribe al flujo estandar de salida
+            ##
+
+            if (pos == 3):
+                cadena ="{}\t1\n".format(word)
+                pos = 0
+                break
+        sys.stdout.write(cadena)
